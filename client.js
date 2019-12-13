58756d233c49cbc3c15ba9f1a527333778b37d1b
Gabriel Hawk <gabriel.thomas.hawk@gmail.com>
2019-12-13 16:09:03 -0600

    Base and most of stretch completed

--- client.js
+++ client.js
@@ -1,10 +1,15 @@
 $(document).ready(startScript);
 
+let totalAnnualSalary = 0;
+
 function startScript() {
-    $('#submitEmployee').on('click',addEmployee);
-    
-    
-    
+    $('#submitEmployee').on('click', addEmployee);
+    $('tbody').on('click', '.deleteEmployeeButton', deleteCurrentEmployee);
+
+}
+
+function addToTotalAnnualSalary(numberToAdd) {
+    totalAnnualSalary += numberToAdd;
 }
 
 function addEmployee() {
@@ -13,21 +18,56 @@ function addEmployee() {
     let idNumber = $('#idNumber').val();
     let jobTitle = $('#jobTitle').val();
     let annualSalary = $('#annualSalary').val();
+    addToTotalAnnualSalary(Number(annualSalary));
 
-    console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
-    
+    displayTotalCost();
+
+    // console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
+
     $('#employeeDataTable').append(`
     <tr class='employee'>
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${idNumber}</td>
         <td>${jobTitle}</td>
-        <td>${annualSalary}</td>
+        <td class="individualEmployeeSalary">${annualSalary}</td>
+        <td><button class='deleteEmployeeButton'>Delete</button></td>
     </tr>`);
+
+    checkIfTotalCostTooHigh();
+
+
+}
+
+function deleteCurrentEmployee() {
     
-    
-    
-    
-    
-    
+    let deletedEmployeeSalary = $(this).closest('tr').find('.individualEmployeeSalary').text();
+    deletedEmployeeSalary = Number(deletedEmployeeSalary);
+    console.log(deletedEmployeeSalary);
+
+    addToTotalAnnualSalary(-deletedEmployeeSalary);
+    checkIfTotalCostTooHigh();
+    displayTotalCost();
+    $(this).closest('tr').remove();
+
+
+}
+
+function displayTotalCost() {
+    let totalCostDisplay = $('#totalCostSpace');
+    totalCostDisplay.empty();
+    totalCostDisplay.append(`<p id='totalAnnualCost'>Total Annual Cost: ${totalAnnualSalary}</p>`);
+}
+
+function checkIfTotalCostTooHigh() {
+    if (totalAnnualSalary > 20000) {
+        $('#totalCostSpace').addClass('tooHigh');
+        $('#totalCostSpace').removeClass('justRight');
+
+    }
+    else {
+        $('#totalCostSpace').removeClass('tooHigh');
+        $('#totalCostSpace').addClass('justRight');
+    }
+
 }
